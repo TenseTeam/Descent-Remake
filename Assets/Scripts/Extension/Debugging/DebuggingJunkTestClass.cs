28b3e73a5aa85d0fe9b64305e533441b90108a56
Denis <zvoyder@protonmail.com>
2023-03-16 07:53:03 +0100

    Classes optimization

---
+++ Assets/Scripts/Extension/Debugging/DebuggingJunkTestClass.cs
@@ -0,0 +1,18 @@
+﻿#if DEBUG && UNITY_EDITOR
+
+using Extension.Finders;
+using UnityEngine;
+
+public class DebuggingJunkTestClass : MonoBehaviour
+{
+    float a = 50;
+    GameObject go;
+
+    private void Start()
+    {
+        go = new GameObject("a");
+        go.DestroyLastComponentOfType<Collider>();
+    }
+}
+
+#endif
