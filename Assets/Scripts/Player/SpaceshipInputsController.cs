d077f0c0fa63d6e3c4d431bc2132a2cd57863bcc
Denis <zvoyder@protonmail.com>
2023-03-20 19:35:20 +0100

    optimizing and fixing movement

--- Assets/Scripts/Player/SpaceshipInputsController.cs
+++ Assets/Scripts/Player/SpaceshipInputsController.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SpaceshipInputsController : MonoBehaviour
@@ -10,6 +9,11 @@ public class SpaceshipInputsController : MonoBehaviour
         Normal = 1,
         Inverted = -1
     }
+
+    [SerializeField]
+    private float _rotatationLerpVelocity;
+
+    private Vector3 rotation;
 
     [Header("Axes")]
     public AxisType pitchAxis = AxisType.Normal;
@@ -24,19 +28,36 @@ public class SpaceshipInputsController : MonoBehaviour
     {
         _inputs = new Inputs();
         _inputs.Enable();
+        StartCoroutine(CalculateRotation());
     }
 
-    internal Vector3 MovementAxis => _inputs.Aircraft.Movement.ReadValue<Vector3>();
+    public Vector3 MovementAxis => _inputs.Aircraft.Movement.ReadValue<Vector3>();
 
-    internal float Yaw => _inputs.Aircraft.Yaw.ReadValue<float>() * (float)yawAxis;
+    public float Yaw => rotation.y;
 
-    internal float Roll => _inputs.Aircraft.Roll.ReadValue<float>() * (float)rollAxis;
+    public float Roll => rotation.z;
 
-    internal float Pitch => _inputs.Aircraft.Pitch.ReadValue<float>() * (float)pitchAxis;
+    public float Pitch => rotation.x;
 
-    internal bool IsRolling => Roll != 0;
-    internal bool IsPitching => Pitch != 0;
-    internal bool IsYawing => Yaw != 0;
+    public bool IsRolling => Roll != 0;
+    public bool IsPitching => Pitch != 0;
+    public bool IsYawing => Yaw != 0;
+    public bool IsRotating => IsRolling || IsPitching || IsYawing;
 
-    internal bool IsRotating => IsRolling || IsPitching || IsYawing;
+    private IEnumerator CalculateRotation()
+    {
+        while (true)
+        {
+            Vector3 rot = new Vector3
+                (
+                _inputs.Aircraft.Pitch.ReadValue<float>() * (float)pitchAxis,
+                _inputs.Aircraft.Yaw.ReadValue<float>() * (float)yawAxis,
+                _inputs.Aircraft.Roll.ReadValue<float>() * (float)rollAxis
+                );
+
+            rotation = Vector3.Lerp(rotation, rot, _rotatationLerpVelocity * Time.deltaTime);
+
+            yield return new WaitForEndOfFrame();
+        }
+    }
 }
