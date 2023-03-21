80656b55ca645fd07d326163c016d267793bf0fa
_Jack <jacopoalberti363@gmail.com>
2023-03-21 10:34:52 +0100

    initial player stats and powerups 3rd patch

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -5,30 +5,37 @@ using UnityEngine;
 public class PlayerStats : MonoBehaviour
 {
     public int PlayerShield = 100;
+    public int ShieldRecharge = 50;
     [Tooltip("Max shield reachable")]
     public int MaxShield = 150;
+    //==========================
     public int PlayerLives;
+    //==========================
     public int PlayerEnergy = 100;
+    public int EnergyRecharge = 100;
     [Tooltip("Max energy reachable")]
     public int MaxEnergy = 200;
+
+    //========================================================= Recharge system pickup
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Sield Recharge"))
         {
-            if (PlayerShield < MaxShield)
-            {
-                PlayerShield = MaxShield;
-
-            }
+            RechargeStat(PlayerShield, ShieldRecharge, MaxShield);
         }
         if (other.gameObject.CompareTag("Energy Recharge"))
         {
-            if (PlayerEnergy < MaxShield)
-            {
-                PlayerEnergy = MaxEnergy;
-
-            }
+            RechargeStat(PlayerEnergy, EnergyRecharge, MaxEnergy);
+        }
+    }
+    private void RechargeStat(int baseStat, int rechargeValue, int maxStat)
+    {
+        baseStat += rechargeValue;
+        if (baseStat > maxStat)
+        {
+            baseStat = maxStat;
         }
     }
 }
+
