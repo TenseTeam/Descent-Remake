4a00a9682f7a88fc78471bf0cc4c21075664c9a4
Denis <zvoyder@protonmail.com>
2023-03-29 01:58:38 +0200

    Unlockables and levels upgrade triggers

---
+++ Assets/Scripts/Item System/Items/Weapons/LevelTriggers/LevelUpHomingMissile.cs
@@ -0,0 +1,25 @@
+﻿
+namespace ProjectDescent.ItemSystem.Items.Weapons
+{
+    using UnityEngine;
+    using ItemSystem.Items.Bullets;
+
+
+    public class LevelUpHomingMissile : MonoBehaviour
+    {
+        [field: SerializeField]
+        public uint LevelsToIncrease { get; set; }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.TryGetComponent(out HomingMissile weap))
+            {
+                if (weap.enabled)
+                    weap.IncreaseLevel(LevelsToIncrease);
+                else
+                    weap.enabled = true;
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Item System/Items/Weapons/LevelTriggers/LevelUpLaser.cs
@@ -0,0 +1,25 @@
+﻿
+namespace ProjectDescent.ItemSystem.Items.Weapons
+{
+    using UnityEngine;
+    using ItemSystem.Items.Bullets;
+
+
+    public class LevelUpLaser : MonoBehaviour
+    {
+        [field: SerializeField]
+        public uint LevelsToIncrease { get; set; }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.TryGetComponent(out LaserCannon weap))
+            {
+                if (weap.enabled)
+                    weap.IncreaseLevel(LevelsToIncrease);
+                else
+                    weap.enabled = true;
+                Destroy(gameObject);
+            }
+        }
+    }
+}
