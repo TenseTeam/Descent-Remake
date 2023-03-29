0fcd2acb4824181cbe70f437c024f116944a498e
Denis <zvoyder@protonmail.com>
2023-03-29 23:55:36 +0200

    added drops

--- Assets/Scripts/AI/Entity/EnemyEntity.cs
+++ Assets/Scripts/AI/Entity/EnemyEntity.cs
@@ -1,10 +1,16 @@
 namespace ProjectDescent.AI.Behaviours
 {
+    using System.Collections.Generic;
     using ProjectDescent.EntitySystem;
     using UnityEngine;
 
     public class EnemyEntity : EntityBase
     {
+        [field: SerializeField, Header("Items Drop")]
+        public List<GameObject> PossibleItemsDrop { get; private set; }
+        [field: SerializeField, Range(0, 100)]
+        public float DropRate { get; set; }
+
         [field: SerializeField, Header("VFXOnDeath")]
         public GameObject VFXExplosion { get; private set; }
 
@@ -15,6 +21,12 @@ namespace ProjectDescent.AI.Behaviours
         {
             Instantiate(VFXExplosion, transform.position, transform.rotation);
             ScoreSingleton.instance.AddScore(ScoreToAddOnKill);
+
+            if(Random.Range(0, 100) < DropRate)
+            {
+                Instantiate(PossibleItemsDrop[Random.Range(0, PossibleItemsDrop.Count)], transform.position, transform.rotation);
+            }
+
             base.Death();
         }
     }
