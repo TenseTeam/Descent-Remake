f47adb2330b6ab69f55e3f646b95a73de0d98b3d
Denis <zvoyder@protonmail.com>
2023-03-31 04:49:34 +0200

    modified setcursor added sprite for cursor

--- Assets/Scripts/Utility/SetCursor.cs
+++ Assets/Scripts/Utility/SetCursor.cs
@@ -6,11 +6,15 @@ public class SetCursor : MonoBehaviour
 {
     public bool isVisibile = false;
     public CursorLockMode cursorLockMode;
+    public Texture2D cursorTexture;
 
 
     void Awake()
     {
         Cursor.visible = isVisibile;
         Cursor.lockState = cursorLockMode;
+
+        if(cursorTexture)
+            Cursor.SetCursor(cursorTexture, Vector2.zero, CursorMode.Auto);
     }
 }
